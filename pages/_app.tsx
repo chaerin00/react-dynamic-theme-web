81ddba7ebe266d898786525cf1f4db000c4e8a04
chaerin <achrvv@sookmyung.ac.kr>
2021-10-20 14:18:11 +0900

    feat: initial setting

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app'
+import { ReactElement } from 'react'
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { ReactElement } from 'react'
+
+const Home: NextPage = (): ReactElement => {
+  return <div></div>
+}
+
+export default Home
