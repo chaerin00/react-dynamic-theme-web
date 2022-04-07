3fb4737f033a685cee7402feb487eda4ffb99524
chaerin <achrvv@sookmyung.ac.kr>
2022-04-07 15:14:19 +0900

    feat: add main sub color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next'
 import { ReactElement } from 'react'
 import { Header } from '@components/common'
 import { fetchTheme, Theme } from 'lib/api/theme'
-import { GetServerSideProps } from 'next'
 import MainLogo from '@components/store/list/MainLogo'
+import { Global, css } from '@emotion/react'
 
 type Props = {
   theme: Theme
 }
 
 const Home: NextPage<Props> = ({ theme }): ReactElement => {
+  const cssVariable = css`
+    :root {
+      --main: ${theme.main_color};
+      --sub: ${theme.sub_color};
+    }
+  `
+
   return (
     <div>
+      <Global styles={cssVariable} />
       <Header theme={theme} />
       <MainLogo mainLogo={theme.main_logo} />
     </div>
@@ -20,7 +28,6 @@ const Home: NextPage<Props> = ({ theme }): ReactElement => {
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const theme = await fetchTheme(req.headers.referer)
-
   return {
     props: {
       theme,
--- styles/palette.ts
+++ styles/palette.ts
@@ -2,13 +2,6 @@ import { css } from '@emotion/react'
 
 const palette = css`
   :root {
-    --main: #ff0000;
-    --primary: #fdcb02;
-    --primary_light: #ffe371;
-    --primary_lighter: #ffeeba;
-    --primary_soft: #f8d05f;
-    --primary_dark: #fcb34a;
-    --primary_darker: #f29c4a;
     --white: #ffffff;
     --lightgray1: #f8f7f6;
     --lightgray2: #dfdfdf;
@@ -22,12 +15,6 @@ const palette = css`
     --bg_yellow: #fffcf1;
     --bg_gray: #f8f8f8;
     --bg_sky: #f0f8fe;
-    --sky_blue: #73acff;
-    --marine_blue: #3c89ff;
-    --mina_blue: #296ce1;
-    --deep_blue: #0e57e2;
-    --honest_blue: #003089;
-    --error_red: #f2754e;
   }
 `
 
