857fcfd8ea69bd7db3b0b6c98349fe95750b6dec
chaerin <achrvv@sookmyung.ac.kr>
2022-04-09 14:38:59 +0900

    fix: move axios interceptor to client.ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ReactElement, useEffect, useState } from 'react'
-import axios from 'axios'
 import { Global } from '@emotion/react'
 import globalStyle from 'styles/globalStyle'
-import router from 'next/router'
 import { QueryClientProvider, QueryClient } from 'react-query'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  axios.interceptors.response.use(
-    (response) => {
-      return response
-    },
-    (error) => {
-      if (error.response.status === 401) {
-        router.push('/login')
-      }
-      return error
-    }
-  )
   const queryClient = new QueryClient()
   return (
     <div>
