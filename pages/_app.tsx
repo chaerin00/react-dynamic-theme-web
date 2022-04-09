e25643d4135f0e87623d0683dd973550a32d6941
chaerin <achrvv@sookmyung.ac.kr>
2022-04-09 14:39:44 +0900

    feat: add theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import type { AppProps } from 'next/app'
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement } from 'react'
 import { Global } from '@emotion/react'
 import globalStyle from 'styles/globalStyle'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { ThemeProvider } from '@contexts/ThemeContext'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   const queryClient = new QueryClient()
   return (
     <div>
-      <QueryClientProvider client={queryClient}>
-        <Global styles={globalStyle} />
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <Global styles={globalStyle} />
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
     </div>
   )
 }
